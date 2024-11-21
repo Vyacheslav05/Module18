b552e754e2820e35f224e5565e45276dd6a647ec
Vyacheslav05 <vyacheslav.lobanov@wellpack.org>
2024-11-21 15:45:54 +0300

    Initial commit

--- UrbanDjango/UrbanDjango/urls.py
+++ UrbanDjango/UrbanDjango/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for UrbanDjangoProject project.
+URL configuration for UrbanDjango project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -14,23 +14,23 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
-from django.views.generic import TemplateView
-# from UrbanDjangoProject.task2.views import func_templ, class_templ
-from UrbanDjango.task2.views import func_templ, class_templ
-
-urlpatterns = [
-    path('func/', func_templ),
-    path('class/', class_templ.as_view()),
-]
-
 # from django.contrib import admin
 # from django.urls import path
+# from django.views.generic import TemplateView
 # from UrbanDjango.task2.views import func_templ, class_templ
 #
 # urlpatterns = [
 #     path('admin/', admin.site.urls),
-#     path('func_templ/', func_templ, name='func_templ'),
-#     path('class_templ/', class_templ.as_view(), name='class_templ'),
+#     path('func/', func_templ),
+#     path('class/', class_templ.as_view()),
 # ]
+
+from django.contrib import admin
+from django.urls import path
+from task2.views import func_templ, class_templ
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('func_templ/', func_templ, name='func_templ'),
+    path('class_templ/', class_templ.as_view(), name='class_templ'),
+]
--- UrbanDjango/task2/views.py
+++ UrbanDjango/task2/views.py
@@ -3,7 +3,7 @@ from django.views.generic import TemplateView
 
 # Create your views here.
 def func_templ(request):
-    return render(request, 'func_template.html')
+    return render(request, 'second_task/func_template.html')
 
 class class_templ(TemplateView):
-    template_name = 'class_template.html'
+    template_name = 'second_task/class_template.html'

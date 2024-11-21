f433a7a0903407e903048cc818408e91b8822e1f
Vyacheslav05 <vyacheslav.lobanov@wellpack.org>
2024-11-21 13:29:31 +0300

    Initial commit

--- UrbanDjango/UrbanDjangoProject/urls.py
+++ UrbanDjango/UrbanDjangoProject/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for UrbanDjango project.
+URL configuration for UrbanDjangoProject project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -16,7 +16,21 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.views.generic import TemplateView
+# from UrbanDjangoProject.task2.views import func_templ, class_templ
+from UrbanDjango.task2.views import func_templ, class_templ
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('func/', func_templ),
+    path('class/', class_templ.as_view()),
 ]
+
+# from django.contrib import admin
+# from django.urls import path
+# from UrbanDjango.task2.views import func_templ, class_templ
+#
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('func_templ/', func_templ, name='func_templ'),
+#     path('class_templ/', class_templ.as_view(), name='class_templ'),
+# ]
